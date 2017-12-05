7089130ccc4ae5193592cd6e40448324919abd73
irshad8in <mshahedullah@newtglobal.com>
2017-12-05 18:24:32 +0530

    initial commit

---
+++ src/constants/ApplicationConstants.java
@@ -0,0 +1,11 @@
+package constants;
+
+
+/**
+ * @date 
+ * @author 
+ * @description Interface class used to store APPLICATION CONSTANTS
+ */
+public interface ApplicationConstants {
+	static final String Perfecto_Report ="Reports/";	
+}
